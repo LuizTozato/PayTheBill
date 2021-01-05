3b27987a8777394600b8d9cf8e346b44dd066efd
Luiz Felipe Neves Tozato <lfntozato@gmail.com>
2021-01-05 19:06:39 -0300

    Add first shopping list

--- app/src/main/java/com/ugps/paythebill/SegundaActivity.java
+++ app/src/main/java/com/ugps/paythebill/SegundaActivity.java
@@ -3,13 +3,25 @@ package com.ugps.paythebill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SegundaActivity extends AppCompatActivity {
 
     private TextView servidor1;
     private TextView servidor2;
+
+    private EditText editValor;
+    private EditText editDescrição;
+
+    private ListView listGastos;
+    private ArrayList<String> itens = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,25 @@ public class SegundaActivity extends AppCompatActivity {
 
         servidor1.setText(nome1);
         servidor2.setText(nome2);
+
+        //================ COLOCANDO VALOR NA LISTA ====================
+
+        listGastos = findViewById(R.id.listGastos);
+        Button buttonAdicionar = findViewById(R.id.buttonAdicionar);
+        editValor = findViewById(R.id.editValor);
+        editDescrição = findViewById(R.id.editDescrição);
+
+        buttonAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = editDescrição.getText().toString() + " - " + editValor.getText().toString();
+                itens.add(str);
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, itens);
+                listGastos.setAdapter(arrayAdapter);
+
+            }
+        });
 
     }
 }
